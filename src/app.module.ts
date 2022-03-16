6b5d60b49b7c93341465c7606507c5594bb5556a
Anderson Souza <andersonmarrone@hotmail.com>
2022-03-16 16:58:24 -0300

    projetot

---
+++ src/app.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+
+import { TasksModule } from './tasks/tasks.module';
+import { UsersModule } from './users/users.module';
+
+@Module({
+  imports: [
+    MongooseModule.forRoot('mongodb+srv://USER:PASSWORD@cluster0-sqxxz.gcp.mongodb.net/DATABASE?retryWrites=true&w=majority'),
+    TasksModule,
+    UsersModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule { }
